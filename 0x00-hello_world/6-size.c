809138833a521b8484a8b8046520cf74738dbce4
Adetola Bewaji <adetola013@gmail.com>
2022-06-02 16:37:07 -0700

    long long int type added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,8 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of a int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
-	printf("Size of a double: %ld byte(s)\n", sizeof(double));
-	printf("Size of a long double: %ld byte(s)", sizeof(long double));
 	return (0);
 }
 
