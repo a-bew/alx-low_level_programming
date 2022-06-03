61e0c6f1cc66ce55007f630880ad8a30fbcfece7
Adetola Bewaji <adetola013@gmail.com>
2022-06-03 16:30:55 -0700

    remove space at start of line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,16 +7,14 @@
  */
 int main(void)
 {
-    char c = 'a';
-
-    while (c <= 'z')
-    {
-        putchar(c);
-        c++;
-    }
-
-    putchar('\n');
-
-    return (0);
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
 
