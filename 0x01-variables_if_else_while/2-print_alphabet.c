66ecc28fa59969161b62e9e3a9fc6fcc490b319f
Adetola Bewaji <adetola013@gmail.com>
2022-06-03 16:18:42 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print alphabet in lowercase, followed by a newline
+ *  * Return 0
+ */
+int main(void)
+{
+    char c = 'a';
+
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
+
