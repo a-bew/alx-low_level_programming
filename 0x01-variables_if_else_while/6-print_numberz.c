8d7e6fc3e7baae8b14c915e6afba1ac3dc6a5d6f
Adetola Bewaji <adetola013@gmail.com>
2022-06-03 19:53:45 -0700

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print single digit of base 10,
+ * starting from 0 followed by a newline
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 0;
+
+	while (c <= '9')
+	{
+		putchar(48 +c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
