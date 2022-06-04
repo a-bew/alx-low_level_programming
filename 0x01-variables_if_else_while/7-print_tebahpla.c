8da8e7b331eb9508cef469be3cfa0de56131fa7b
Adetola Bewaji <adetola013@gmail.com>
2022-06-03 20:09:02 -0700

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print alphabet in lowercase in reverse, followed by a newline
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
