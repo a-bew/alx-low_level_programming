506db13f3f9c09236508e1d1fd0ae145a93daab1
Adetola Bewaji <adetola013@gmail.com>
2022-06-08 12:36:13 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int _isalpha(char c)
+{
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
