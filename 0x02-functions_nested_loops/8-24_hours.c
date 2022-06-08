d0164d333ea38a2a7b0bd5d57e27aea6458ecabe
Adetola Bewaji <adetola013@gmail.com>
2022-06-08 16:36:32 -0700

    24 hours updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,20 +8,29 @@
 void jack_bauer(void)
 {
 	int hour = 0;
-	
+	char hourDiv, minuteDiv;
+
 	while (hour < 24)
 	{
 		int minute = 0;
 		
 		while (minute < 60)
 		{
-			printf("%s%d:%s%d\n", hour / 10 >= 1 ? "" : "0", hour, minute / 10 >= 1 ? "" : "0", minute);
-            minute++;
-        }
-        hour++;
-    }
+			hourDiv =  hour / 10 >= 1 ? "": "0";
+			minuteDiv = minute / 10 >= 1 ? "" : "0";
 
-    return (0);
+			_putchar('0' + hourDiv);
+			_putchar('0' + hour);
+			_putchar(":");
+			_putchar('0' + minuteDiv);
+			_putchar('0' + minute);
+			
+			minute++;
+		}
 
+		hour++
+	}
+
+	return (0);
 }
 
