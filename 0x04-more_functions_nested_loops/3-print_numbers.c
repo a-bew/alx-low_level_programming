90868317abf2fe75d59a4487b2bb564d03dd7bf5
Adetola Bewaji <adetola013@gmail.com>
2022-06-09 08:06:38 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_numbers - Entry point
+* description - prints  the numbers, from 0 to 9, followed by a new line
+* Return: void
+*/
+void print_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
