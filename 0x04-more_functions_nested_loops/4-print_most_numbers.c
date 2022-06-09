5581468aec6a3433994992abe6ef3b93f55f5475
Adetola Bewaji <adetola013@gmail.com>
2022-06-09 08:23:39 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_most_numbers - Entry point
+* description - prints  the numbers, from 0 to 9, followed by a new line
+* Return: void
+*/
+void print_most_numbers(void)
+{
+	char c = 0;
+
+	while (c <= 9)
+	{
+		if (c != 2 || c!= 4)
+		{
+			_putchar('0' + c);
+		}
+		c++;
+	}
+	_putchar('\n');
+}
+
