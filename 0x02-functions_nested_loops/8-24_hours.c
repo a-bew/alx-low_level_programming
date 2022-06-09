2af9838e988754a527e618616a5b0db9d1df6a19
Adetola Bewaji <adetola013@gmail.com>
2022-06-08 19:56:40 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,36 +2,50 @@
 
 /**
  * jack_bauer - jack bauer
- * Return: always 0
+ * Return: void
  */
 
 void jack_bauer(void)
 {
-	int hour = 0;
-	char hourDiv; 
-	char minuteDiv;
-
-	while (hour < 24)
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+	int a = 9;
+	
+	h2 = 0;
+	
+	while (h2 <= 2)
 	{
-		int minute = 0;
-		
-		while (minute < 60)
+		if (h2 == 2)
 		{
-			hourDiv =  hour / 10 >= 1 ? \'\':\'0\';
-			minuteDiv = minute / 10 >= 1 ? \'\' : \'0\';
-
-			_putchar('0' + hourDiv);
-			_putchar('0' + hour);
-			_putchar(':');
-			_putchar('0' + minuteDiv);
-			_putchar('0' + minute);
-			
-			minute++;
+			/*Restrain to 23h, not 29*/
+			a = 3;
 		}
-
-		hour++
+		h1 = 0;
+		while (h1 <= a)
+		{
+			m2 = 0;
+			while (m2 <= 5)
+			{
+				m1 = 0;
+				while (m1 <= 9)
+				{
+					_putchar('0' + h2);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m2);
+					_putchar('0' + m1);
+					_putchar('\n');
+					
+					m1++;
+				}
+				
+				m2++;
+			}
+			h1++;
+		}
+		h2++;
 	}
-
-	return (0);
 }
 
