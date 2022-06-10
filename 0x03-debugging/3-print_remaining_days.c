75296d169879faf7382e31dbf35df4dc71c2710e
Adetola Bewaji <adetola013@gmail.com>
2022-06-10 07:21:56 -0700

    indentation added

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,10 +22,10 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-	else if (((year % 4 == 0) && (year % 100!= 0)) || (year % 400 == 0))
+	else if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
 		printf("Day of the year: %d\n", day);
-                printf("Remaining days: %d\n", 366 - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
