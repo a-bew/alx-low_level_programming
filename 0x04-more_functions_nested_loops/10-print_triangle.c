242d6c9cb3631bbf05d193a71a7797b0c9fe933c
Adetola Bewaji <adetola013@gmail.com>
2022-06-09 21:14:40 -0700

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* print_diagonal - Entry point
+* @b: int type number
+* Return: void
+*/
+void print_triangle(int b)
+{
+	char d = '#';
+	int c, e;
+	int a = 0;
+
+	while (a < b)
+	{
+		int n;
+		for (n = 0; n <= b; n++)
+		{
+			c = b - a;
+			if (c > n)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(d);
+			}
+		}
+		a++;
+		_putchar('\n');
+	}
+}
+
