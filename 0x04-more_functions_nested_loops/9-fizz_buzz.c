f4948e82b70453a8e176f2c470f87284c7e03cdd
Adetola Bewaji <adetola013@gmail.com>
2022-06-09 20:45:58 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - main
+ * Return:0
+ */
+
+int main()
+{
+	int n;
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0 && n % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		if (n < 100)
+		{
+			printf(" ");
+		}
+	}
+	return (0)
+}
+
