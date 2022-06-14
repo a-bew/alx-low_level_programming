df567cc220f4bb992565920ff211896fff8c15de
Adetola Bewaji <adetola013@gmail.com>
2022-06-14 02:49:58 -0700

    description updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev - prints a string in reversal, followed by a new line,
+ * print_rev - prints a string in reversal, followed by a new line,
  * @s: pointer to the string to print
  * Return: void
 */
