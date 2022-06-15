0f6a79eec0e23949d8f7a2fa8421edfad8cbba79
Adetola Bewaji <adetola013@gmail.com>
2022-06-14 21:06:27 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line,
+ * puts_half - prints a string, followed by a new line,
  * @s: pointer to the string to print
  * Return: void
 */
 
 
-void _puts(char *s)
+void puts_half(char *s)
 {
 	int len = 0;
 	int r, n, start;
