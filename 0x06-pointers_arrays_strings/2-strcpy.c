3bbcf28e974d9b05c045c1c05686fed9b149d23b
Adetola Bewaji <adetola013@gmail.com>
2022-06-17 12:36:12 -0700

    _strn copy

--- 0x06-pointers_arrays_strings/2-strcpy.c
+++ 0x06-pointers_arrays_strings/2-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * *_strncpy - find the length of a string
  * @dest: pointer to the string
@@ -7,16 +8,17 @@
  * Return: destination
 */
 
-
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	char i;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[i] = src[i];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
 
-for ( ; i < n; i++)
-	dest[i] = '\0';
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
-return (dest);
+	return (dest);
 }
