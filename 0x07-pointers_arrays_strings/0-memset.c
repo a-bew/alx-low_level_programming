6ed3f3e7e7f92c9cd5bd13b6138a45efae1415f0
Adetola Bewaji <adetola013@gmail.com>
2022-06-20 19:31:02 -0700

    fills a memory block with a constant byte - updates

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 include "main.h"
 
 /**
- * _memset - fills a memory block with a constant byte
+ * *_memset - fills a memory block with a constant byte
  * @s: address to memory block
  * @b: char to be used
  * @n: number of bytes to be used
@@ -11,10 +11,14 @@ include "main.h"
 
 char *_memset(char *s, char b, unsigned int n)
 {
+
 	while (n)
+
 	{
 		s[n - 1] = b;
 		n--;
 	}
+
 	return (s);
+
 }
