412e2b37b108226c5a427786ba00c3587f13fd85
Adetola Bewaji <adetola013@gmail.com>
2022-06-24 21:11:58 -0700

    prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints all arguments it receives
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int start = 1;
+
+	while (start < argc)
+	{
+		printf("%s\n", argv[start]);
+		start++;
+	}
+
+	return (0);
+
