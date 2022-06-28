90982a115d0a61a99edefe253acf9bf467710faa
Adetola Bewaji <adetola013@gmail.com>
2022-06-28 14:09:10 -0700

    main.h updated

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,15 @@
 #include "main.h"
+/**
+ * main - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * On success: return the number of characters printed
+ */
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to \
+		    be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
