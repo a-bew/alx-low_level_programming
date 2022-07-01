e00eeece085c1eabf5a4a36b8f12802b826e00c3
Adetola Bewaji <adetola013@gmail.com>
2022-07-01 14:35:59 -0700

    remove trailing space

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int i;
 	char *p;
-	
+
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	i = 0;
-	
+
 	for (i = 0; i < nmemb; i++)
 	{
 		p[i] = 0;
