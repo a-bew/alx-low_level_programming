192cc16a4c58d1f0d521f26cb56f4492ce39118b
Adetola Bewaji <adetola013@gmail.com>
2022-07-01 03:10:26 -0700

    exit description = 98

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,12 +4,14 @@
  * *malloc_checked - allocate memory using malloc
  * @b - amount of bytes
  * Return: pointer to the allocated memory
+ * if malloc fails, status value = 98
  */
 
 void *malloc_checked(unsigned int b)
 {
-	char *c = malloc(b);
+	char *p;
 
+	p = malloc(b);
 	if (c == NULL)
 		exit(98);
 
