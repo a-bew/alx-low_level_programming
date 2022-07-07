0d57f341cbab32f0a549ed7740d29fe5772550f3
Adetola Bewaji <adetola013@gmail.com>
2022-07-06 18:33:00 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: function which print name
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
