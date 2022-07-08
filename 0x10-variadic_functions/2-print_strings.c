24255bf37efe5bd5c693af665c578638ba0ef185
Adetola Bewaji <adetola013@gmail.com>
2022-07-07 23:10:52 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,23 +11,25 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list list;
-unsigned int i;
-char *c;
-va_start(list, n);
+	va_list list;
+	unsigned int i;
+	char *c;
 
-for (i = 0; i < n; i++)
-{
-	c = va_arg(list, char*);
-	if (c != NULL)
-		printf("%s", c);
-	else
-		printf("(nil)");
+	va_start(list, n);
 
-	if (i != n - 1 && separator != NULL)
-		printf("%s", separator);
-}
-va_end(list);
+	for (i = 0; i < n; i++)
+	{
+		c = va_arg(list, char*);
+		if (c != NULL)
+			printf("%s", c);
+		else
+			printf("(nil)");
 
-putchar('\n');
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+
+	va_end(list);
+
+	putchar('\n');
 }
