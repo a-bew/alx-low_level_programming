69bb2c149104480703572850d39a1f1437f36db3
Adetola Bewaji <adetola013@gmail.com>
2022-07-07 22:47:52 -0700

    print number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,18 +11,18 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list list;
-unsigned int i;
+	va_list list;
+	unsigned int i;
 
-va_start(list, n);
+	va_start(list, n);
 
-for (i = 0; i < n; i++)
-{
-	printf("%i", va_arg(list, int));
-	if (i != n - 1 && separator != NULL)
-		printf("%s", separator);
-}
-va_end(list);
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", va_arg(list, int));
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
 
-putchar('\n');
+	va_end(list);
+	putchar('\n');
 }
