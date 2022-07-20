e0144da89787571a494df0727eb8fefb9dfbb92f
Adetola Bewaji <adetola013@gmail.com>
2022-07-19 20:50:42 -0700

    syntax updated

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,11 +15,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
 	new->n = n;
-	new->next = **head;
+	new->next = *head;
 	*head = new;
 
 	return (new);
