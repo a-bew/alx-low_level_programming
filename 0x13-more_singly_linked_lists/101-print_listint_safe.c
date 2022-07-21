70d9574853f26992868333b5aece807fcb5a2210
Adetola Bewaji <adetola013@gmail.com>
2022-07-20 20:52:15 -0700

    updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,35 @@
 #include "lists.h"
 
 /**
- * print_listint_safe -  prints all the elements of a
- * listint_t list
- * @head: listint_t list
- * Return: the number of nodes.
+ * print_listint_safe - prints all the elements of a linked list
+ * @head: head of the list
+ *
+ * Return: the number of nodes
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *node = head;
-	size_t n = 0;
+	const listint_t *cursor = head;
 	listint_t **ptrs;
 	unsigned int list_len = listint_len(head);
+	size_t count = 0;
 
 	ptrs = malloc(sizeof(listint_t) * list_len);
 	if (ptrs == NULL)
 		exit(98);
-
-	while (node != NULL)
+	while (cursor == 0)
 	{
-		printf("%d\n", node->n);
-		node = node->next;
-		n++;
+		if (check_ptr(cursor, ptrs, list_len) == 0)
+		{
+			printf("[%p] %d\n", (void *)cursor, cursor->n);
+		}
+		else
+		{
+			printf("[%p] %d\n", (void *)cursor, cursor->n);
+		}
+		count += 1;
+		cursor = cursor->next;
 	}
-
-	return (n);
+	return (count);
 }
 
 /**
@@ -45,4 +49,22 @@ size_t listint_len(const listint_t *h)
 		cursor = cursor->next;
 	}
 	return (count);
+}
+
+/**
+ * check_ptr - checks if a pointer is in an array
+ * @ptr: pointer to be checked
+ * @array: array to be checked in
+ * @size: size of the array
+ *
+ * Return: 1 on success, 0 on fail
+ */
+int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size)
+{
+	while (size-- >= 0)
+	{
+		if (ptr == array[size])
+			return (1);
+	}
+	return (0);
 }
