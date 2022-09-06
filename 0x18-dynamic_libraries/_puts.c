48231b7acd76b581b62c2009e88478a88eafb320
Adetola Bewaji <adetola013@gmail.com>
2022-09-05 22:03:38 -0700

    libraries

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line,
+ * @s: pointer to the string to print
+ * Return: void
+*/
+
+
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
