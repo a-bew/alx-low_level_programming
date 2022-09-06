f8f4106b6f0e4fd15409fae5822015d5c92a8a3d
Adetola Bewaji <adetola013@gmail.com>
2022-09-05 21:18:13 -0700

    _putchar

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main -Entry point
+* Return: 0
+*/
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
